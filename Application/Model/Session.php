f181fd77cad431b42b60296ba908143522f8833d
chenxb <chenxb@ename.com>
2015-04-22 13:15:25 +0800

    init session

---
+++ Application/Model/Session.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Model;
+use \Mvc\Model;
+
+/**
+ * Session模型
+ */
+class Session extends Model
+{
+	protected $table = "session";
+
+	/**
+	 * 开启session
+	 * @param string 目录
+	 * @param string sessionname
+	 * @return int 1或者false
+	 */
+	public function open($path, $name)
+	{
+		// 执行插入或替换
+		$sql = "REPLACE INTO {$this->table} SET session_id=:id, ip=:ip, lastDate=:date";
+		// sql语句执行
+		$this->db->query($sql, array(':id'=>session_id(), 'ip'=>CLIENT_IP, ':date'=>time()));
+		// 返回影响行数
+		return $this->db->rowCount();
+	}
+
+	/**
+	 * 关闭session
+	 * @return void
+	 */
+	public function close()
+	{
+		return TRUE;
+	}
+
+	/**
+	 * 读取配置
+	 * @param string sessionId
+	 */
+	public function read($id)
+	{
+		return $this->field('data')->where(array('session_id'=>$id))->select(self::FETCH_ROW);
+	}
+
+	/**
+	 * 写入session
+	 * @param string sessionid
+	 * @param 
+	 */
+	public function write($id, $data)
+	{
+		return $this->where(array('session_id'=>$id))->update(array('data'=>$data));
+	}
+
+	/**
+	 * 删除session信息
+	 * @param string sessionid
+	 * @return int
+	 */
+	public function destroy($id)
+	{
+		return $this->where(array('session_id'=>$id))->limit(1)->delete();
+	}
+
+	/**
+	 * 而外的回收机制
+	 * @return void
+	 */
+	public function gc()
+	{
+		return $this->where(array('session_id'=>$id))->limit(1)->delete();
+	}
+}
--- Framework/Storge/Session.php
+++ Framework/Storge/Session.php
@@ -2,13 +2,53 @@
 
 namespace Storge;
 
+use \Core\C;
+
 class Session
 {
 	/**
-	 * 初始化Session存储方式
+	 * 数据库session操作模型
+	 * @var object
 	 */
-	public static function init()
+	private static $model;
+
+	/**
+	 * 初始化Session存储方式
+	 * @return void
+	 */
+	public static function start()
 	{
-		
+		$config = C::session();
+
+		// 设置session名
+		session_name($config->name);
+		// 设置保存方式
+		switch($config->handler)
+		{
+			case 'redis':
+			case 'memcached':
+				ini_set('session.save_handler', $config->handler);
+				$config->path = "tcp://{$config->path}";
+			case 'memcache':
+				ini_set('session.save_path', $config->path);
+				break;
+			case 'mysql':
+				ini_set('session.save_path', 'user');
+				self::$model = new \Model\Session();
+				session_set_save_handler(
+					array(self::$model,'open'),
+					array(self::$model,'close'),
+					array(self::$model,'read'),
+					array(self::$model,'write'),
+					array(self::$model,'destroy'),
+					array(self::$model,'gc')	
+				);
+				break;
+		}
+		// 设置过期时间
+		ini_set('session.gc_maxlifetime', $config->expire);
+
+		// 开启session
+		session_start();
 	}
 }
